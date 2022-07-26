65ea4a00bf04b6ad74d85d6f3898970c176f0eb8
faheelsattar <faheelsattar@yahoo.com>
2022-07-26 10:41:33 +0500

    clean: console removed

--- test/Queue.t.sol
+++ test/Queue.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.4;
 
-import { console } from "forge-std/console.sol";
 import { PRBTest } from "@prb/test/PRBTest.sol";
 
 import { Queue } from "../src/Queue.sol";
